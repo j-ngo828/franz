266529066cac97177c0082f733a9230358413346
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2024-10-01 23:50:31 -0400

    Send a simple response

--- app/server.go
+++ app/server.go
@@ -11,7 +11,7 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
-const SEVEN int32 = 7
+const SEVEN uint32 = 7
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -24,29 +24,29 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	defer l.Close()
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	defer conn.Close() // Ensure the connection is closed properly
 
 	fmt.Println("Successfully set up connection")
 
 	// Read APIVersions requests
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error reading from connection:", err.Error())
-		return
-	}
-	fmt.Printf("Received %d bytes: %s\n", n, string(buf[:n]))
+	// buf := make([]byte, 1024)
+	// n, err := conn.Read(buf)
+	// if err != nil {
+	// 	fmt.Println("Error reading from connection:", err.Error())
+	// 	return
+	// }
+	// fmt.Printf("Received %d bytes: %s\n", n, string(buf[:n]))
 
 	// first 4 bytes are 0, last 4 bytes should represent 7 in big endian byte
 	response := make([]byte, 8)
-	binary.BigEndian.PutUint32(response, uint32(SEVEN))
+	binary.BigEndian.PutUint32(response[4:], SEVEN)
 	conn.Write(response)
 
 	fmt.Println("Connection handled successfully")
+	l.Close()
+	conn.Close() // Ensure the connection is closed properly
 }
