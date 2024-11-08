7f044d1f05ca7f0db4789ab278fe386576b94a75
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2024-11-07 21:55:58 -0500

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -12,12 +12,24 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
-func getCorrelationId(buf []byte) uint32 {
-	return binary.BigEndian.Uint32(buf[8:12])
+func getMessageLen(buf []byte) uint32 {
+	return binary.BigEndian.Uint32(buf[0:4])
+}
+
+func getRequestApiKey(buf []byte) uint16 {
+	return binary.BigEndian.Uint16(buf[4:6])
 }
 
 func getRequestApiVersion(buf []byte) uint16 {
 	return binary.BigEndian.Uint16(buf[6:8])
+}
+
+func getCorrelationId(buf []byte) uint32 {
+	return binary.BigEndian.Uint32(buf[8:12])
+}
+
+func writeMessageLen(res []byte, messageLen uint32) {
+	binary.BigEndian.PutUint32(res[0:4], messageLen)
 }
 
 func writeCorrelationId(res []byte, correlationId uint32) {
@@ -28,12 +40,16 @@ func writeErrorCode(res []byte, errCode uint16) {
 	binary.BigEndian.PutUint16(res[8:10], errCode)
 }
 
+func writeRequestApiKey(res []byte, apiKey uint16) {
+	binary.BigEndian.PutUint16(res[10:12], apiKey)
+}
+
 func createResponse(len int) []byte {
 	return make([]byte, len, 1024)
 }
 
 func main() {
-	validAPIVersions := []uint16{0, 1, 2, 3}
+	validAPIVersions := []uint16{0, 1, 2, 3, 4}
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
@@ -64,19 +80,33 @@ func main() {
 
 	// first 4 bytes are 0, last 4 bytes should represent correlation_id field from request
 	response := createResponse(n)
-
+	fmt.Printf("response:\t %v \n", []byte(response))
 	// NOTE:
 	// read 4 bytes, starting from the 8th byte, since correlation_id is 32 bits.
 	// first 4 bytes from the buffer are for the message length!!
 	correlationId := getCorrelationId(buf)
 	writeCorrelationId(response, correlationId)
+	fmt.Printf("response:\t %v \n", []byte(response))
+
 	requestApiVersion := getRequestApiVersion(buf)
 
 	if !slices.Contains(validAPIVersions, requestApiVersion) {
 		writeErrorCode(response, 35)
+	} else {
+		writeErrorCode(response, 0)
 	}
-	conn.Write(response)
+	fmt.Printf("response:\t %v \n", []byte(response))
 
+	messageLen := getMessageLen(buf)
+	writeMessageLen(response, messageLen)
+	fmt.Printf("response:\t %v \n", []byte(response))
+
+	apiKey := getRequestApiKey(buf)
+	writeRequestApiKey(response, apiKey)
+	fmt.Printf("response:\t %v \n", []byte(response))
+
+	conn.Write(response)
+	fmt.Println("N: %d", n)
 	fmt.Println("Connection handled successfully")
 	l.Close()
 	conn.Close() // Ensure the connection is closed properly
